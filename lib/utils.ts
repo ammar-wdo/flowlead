070cb54d6a777cade27def0b604a332bcce62b80
FLOWLEAD <flowlead@wdodigitals-Mac-mini.local>
2024-05-16 10:18:23 +0400

    auth

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,29 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import bcrypt from "bcryptjs";
+import { CustomError } from "@/custom-error";
+
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+
+export async function comparePasswords(
+  plainPassword: string,
+  hashedPassword: string
+) {
+  try {
+    const passwordMatch = await bcrypt.compare(plainPassword, hashedPassword);
+    return passwordMatch;
+  } catch (error) {
+    throw new CustomError("Error comparing passwords");
+  }
+}
+
+
+export const hashPassword = async (password: string) => {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  return hashedPassword;
+};
