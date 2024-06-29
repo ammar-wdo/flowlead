4c1c6c3fc3a07489ddd6bc29b755cec51102d576
ammar <ammar@wdodigital.com>
2024-06-29 12:37:46 +0400

    weglot

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,20 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
 import ModalsProvider from "@/providers/modals-provider";
-import { EdgeStoreProvider } from '../lib/edgestore';
-import { Toaster } from "@/components/ui/sonner"
-import NextTopLoader from 'nextjs-toploader';
-import 'quill/dist/quill.snow.css'; // Import the snow theme CSS
+import { EdgeStoreProvider } from "../lib/edgestore";
+import { Toaster } from "@/components/ui/sonner";
+import NextTopLoader from "nextjs-toploader";
+import "quill/dist/quill.snow.css"; // Import the snow theme CSS
 import { cn } from "@/lib/utils";
-
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
-const poppins = Poppins({ subsets: ["latin"],weight:["100","200","300","400","500","600","700","800","900"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,22 +29,31 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-     
-
-
-    <html lang="en">
-
-      <body className={cn(poppins.className,"scroll")}>
-      <EdgeStoreProvider>
-      <NextTopLoader   height={4} color="#111725"/>
-        {children}
-      <ModalsProvider/>
-      </EdgeStoreProvider>
-      <Toaster position="top-center" richColors/>
+      
+      <html lang="en">
+      
+      <Script
+          type="text/javascript"
+          src="https://cdn.weglot.com/weglot.min.js"
+        ></Script>
+        <Script>
+          {`
+              Weglot.initialize({
+                api_key: 'wg_7feb4815fb5c361da19061e921d6ab622'
+              });
+            `}
+        </Script>
+    
+      
+        <body className={cn(poppins.className, "scroll")}>
+          <EdgeStoreProvider>
+            <NextTopLoader height={4} color="#111725" />
+            {children}
+            <ModalsProvider />
+          </EdgeStoreProvider>
+          <Toaster position="top-center" richColors />
         </body>
-   
-    </html>
- 
+      </html>
     </ClerkProvider>
   );
 }
