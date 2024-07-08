2b203e0fb20b022dd259b790273963db4466e816
ammar <ammar@wdodigital.com>
2024-07-08 15:10:30 +0400

    refine

--- app/(dashboard)/[companySlug]/page.tsx
+++ app/(dashboard)/[companySlug]/page.tsx
@@ -3,6 +3,7 @@ import prisma from '@/lib/prisma'
 import { checkCompanySubscription } from '@/lib/utils'
 import { UserButton } from '@clerk/nextjs'
 import { auth } from '@clerk/nextjs/server'
+import { format } from 'date-fns'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
@@ -11,11 +12,23 @@ type Props = {params:{companySlug:string}}
 const page =async ({params:{companySlug}}: Props) => {
 const {userId} = auth()
 if(!userId) redirect('/sign-up')
-  
+
   await checkCompanySubscription({userId,companySlug})
+  const company=  await prisma.company.findUnique({
+    where:{
+      slug:companySlug,
+      userId
+    },
+    select:{
+      contactPerson:true
+    }
+  })
  
   return (
-    <div></div>
+    <div>
+<span className='text-muted-foreground'>{format(new Date,"EEE, do MMMM")}</span>
+<h3 className='text-5xl font-bold mt-6'>Hello, {company?.contactPerson}.</h3>
+    </div>
   )
 }
 
