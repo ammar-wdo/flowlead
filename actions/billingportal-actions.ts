d6f4331de627612fb63dff2b40f73a4215656a2b
ammar <ammar@wdodigital.com>
2024-07-08 13:11:42 +0400

    refine

--- actions/billingportal-actions.ts
+++ actions/billingportal-actions.ts
@@ -30,7 +30,7 @@ export const billingPortal = async ({
     // create session
     const session = await stripe.billingPortal.sessions.create({
       customer: company.customerStripeId,
-      return_url: `${process.env.NEXT_PUBLIC_BASE_URL}/${company.slug}/update`,
+      return_url: `${process.env.NEXT_PUBLIC_BASE_URL}/${company.slug}/upgrade`,
       
     });
 
